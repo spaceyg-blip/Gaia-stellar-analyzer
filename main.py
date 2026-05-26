18ab3eb4d7752704c27e08708e82697ec5ed17e0
spaceyg-blip <tech.com.g07@gmail.com>
2026-05-26 16:24:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ from src.hr_diagram import plot_hr
 from src.cluster_stars import cluster_stars
 from src.plot_clusters import plot_clusters
 
-# Load Gaia sample
+from src.tess_lightcurve import load_lightcurve
+from src.find_period import find_period
+from src.phase_fold import phase_fold
+from src.extract_features import extract_features
+
+# -------------------
+# Gaia Section
+# -------------------
+
 data = load_gaia()
 
-# Color index
 bp_rp = data[
     "bp_rp"
 ]
 
-# Absolute magnitude
 abs_mag = (
     data[
         "phot_g_mean_mag"
@@ -33,26 +39,58 @@ abs_mag = (
     5
 )
 
-# HR diagram
 plot_hr(
     bp_rp,
     abs_mag
 )
 
-# Stellar clustering
 table = cluster_stars(
     bp_rp,
-    abs_mag,
-    n_clusters=3
+    abs_mag
 )
 
-# Cluster visualization
 plot_clusters(
     table
 )
 
-# Save Gaia sample
 data.write(
     "gaia_sample.csv",
     overwrite=True
+)
+
+# -------------------
+# TESS Section
+# -------------------
+
+target = "AB Dor"
+
+lc = load_lightcurve(
+    target
+)
+
+time = lc.time.value
+
+flux = lc.flux.value
+
+period, frequency, power = find_period(
+    time,
+    flux
+)
+
+phase = phase_fold(
+    time,
+    period
+)
+
+features = extract_features(
+    flux
+)
+
+print(
+    "Recovered Period:",
+    period
+)
+
+print(
+    features
 )

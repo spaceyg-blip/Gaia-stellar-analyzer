ebdccd076213bce3c9b8e7b523fe0dd0fac747f7
spaceyg-blip <tech.com.g07@gmail.com>
2026-05-27 14:48:30 +0530

    Update find_period.py

--- src/find_period.py
+++ src/find_period.py
@@ -1,23 +1,41 @@
 from astropy.timeseries import LombScargle
+import numpy as np
 
 def find_period(
     time,
     flux
 ):
 
+    mask = (
+        np.isfinite(time)
+        &
+        np.isfinite(flux)
+    )
+
+    time = time[
+        mask
+    ]
+
+    flux = flux[
+        mask
+    ]
+
     frequency, power = LombScargle(
         time,
         flux
     ).autopower(
-        minimum_frequency=0.5,
-        maximum_frequency=5
+        minimum_frequency=0.05,
+        maximum_frequency=2
     )
 
     best_frequency = frequency[
         power.argmax()
     ]
 
-    period = 1 / best_frequency
+    period = (
+        1 /
+        best_frequency
+    )
 
     return (
         period,
